8fe87a222ed23a147c0da678a0d26e9412e7b05d
victorpreston <prestonvictor25@gmail.com>
2024-02-12 20:32:41 -0800

    main header file

--- main.h
+++ main.h
@@ -2,11 +2,21 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+/* maximum length of stirng input */
+#define MAX_LENGTH 100
 
 /**
  * prototypes
  */
 
 void fizzbuzz(int lowerLimit, int upperLimit);
+void fibonacci(int limit);
+bool isPowerOfTwo(int n);
+void capitalize(char* str);
+int reverse(int num);
+int countVowels(char* str);
 
 #endif /* teach2give */
